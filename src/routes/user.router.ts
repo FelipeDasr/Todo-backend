f8bca0eb4233c38f47e516d4566b35e34b7a9b03
FelipeDasr <felipe.heiker07@gmail.com>
2022-05-04 17:05:05 -0300

    New user route (|delete| /user)

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 const userRouter = Router();
 
+// Controllers
+import UserController from '../controllers/User.controller';
+
 //
 import { authChecker } from '../middlewares/authChecker'
+
+userRouter.delete('/user', authChecker, UserController.deleteUser);
 
 export { userRouter }
